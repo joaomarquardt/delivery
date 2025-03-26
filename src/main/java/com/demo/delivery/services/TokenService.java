535eb55870a3b24325dfe78f31b65aaddb95e63f
joaomarquardt <marquardt1402@gmail.com>
2025-03-26 12:20:00 -0300

    feat: add jwt token service to generate and validate tokens

---
+++ src/main/java/com/demo/delivery/services/TokenService.java
@@ -0,0 +1,43 @@
+package com.demo.delivery.services;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.demo.delivery.domain.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
+public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("delivery-api")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(Instant.now().plusSeconds(3600))
+                    .sign(algorithm);
+        } catch (JWTCreationException e) {
+            throw new JWTCreationException("Error while generating token", e);
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("delivery-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException e) {
+            return "";
+        }
+    }
+}
