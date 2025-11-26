22a04be02749179e305f582073062e4d1472739c
joaomarquardt <marquardt1402@gmail.com>
2025-11-26 17:53:36 -0300

    feat: add logic to create an order

--- src/main/java/com/api/delivery/services/OrderService.java
+++ src/main/java/com/api/delivery/services/OrderService.java
@@ -1,6 +1,10 @@
 package com.api.delivery.services;
 
 import com.api.delivery.domain.Order;
+import com.api.delivery.domain.OrderItem;
+import com.api.delivery.domain.OrderStatus;
+import com.api.delivery.domain.Product;
+import com.api.delivery.dtos.requests.OrderItemRequest;
 import com.api.delivery.dtos.requests.CreateOrderRequest;
 import com.api.delivery.dtos.requests.UpdateOrderStatusRequest;
 import com.api.delivery.dtos.responses.OrderResponse;
@@ -9,16 +13,22 @@ import com.api.delivery.repositories.OrderRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class OrderService {
     private final OrderRepository orderRepository;
     private final OrderMapper orderMapper;
+    private final ProductService productService;
 
-    public OrderService(OrderRepository orderRepository, OrderMapper orderMapper) {
+    public OrderService(OrderRepository orderRepository, OrderMapper orderMapper, ProductService productService) {
         this.orderRepository = orderRepository;
         this.orderMapper = orderMapper;
+        this.productService = productService;
     }
 
     public List<OrderResponse> findAll() {
@@ -36,7 +46,25 @@ public class OrderService {
     }
 
     public OrderResponse createOrder(CreateOrderRequest request) {
-        Order order = orderMapper.toOrderEntity(request);
+        BigDecimal totalValue = BigDecimal.valueOf(0);
+        List<OrderItem> items = new ArrayList<>();
+        for (OrderItemRequest itemRequest : request.items()) {
+            Product product = productService.findProductEntityById(itemRequest.productId());
+            totalValue = totalValue.add(product.getPrice());
+            OrderItem orderItem = new OrderItem();
+            orderItem.setAmount(itemRequest.amount());
+            orderItem.setProduct(product);
+            orderItem.setPrice(product.getPrice());
+            items.add(orderItem);
+        }
+        Order order = new Order();
+        order.setTotalValue(totalValue);
+        order.setStatus(OrderStatus.PENDING);
+        order.setOrderedOn(Timestamp.from(Instant.now()));
+        for (OrderItem item : items) {
+            item.setOrder(order);
+        }
+        order.setItems(items);
         Order createdOrder = orderRepository.save(order);
         return orderMapper.toOrderResponse(createdOrder);
     }
