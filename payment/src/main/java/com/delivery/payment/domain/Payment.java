74e025adc69504bcf5ae6f8ec949089339a3b980
joaomarquardt <marquardt1402@gmail.com>
2025-11-29 23:55:51 -0300

    feat: add payment domain with its enums

---
+++ payment/src/main/java/com/delivery/payment/domain/Payment.java
@@ -0,0 +1,71 @@
+package com.delivery.payment.domain;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Entity
+@Table(name = "payments")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long orderId;
+    private BigDecimal value;
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status;
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+    public Payment() {
+    }
+
+    public Payment(Long id, Long orderId, PaymentStatus status, BigDecimal value, PaymentMethod paymentMethod) {
+        this.id = id;
+        this.orderId = orderId;
+        this.status = status;
+        this.value = value;
+        this.paymentMethod = paymentMethod;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public PaymentStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(PaymentStatus status) {
+        this.status = status;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+}
---
+++ payment/src/main/java/com/delivery/payment/domain/PaymentMethod.java
@@ -0,0 +1,8 @@
+package com.delivery.payment.domain;
+
+public enum PaymentMethod {
+    CREDIT_CARD,
+    DEBIT_CARD,
+    PIX,
+    CASH
+}
