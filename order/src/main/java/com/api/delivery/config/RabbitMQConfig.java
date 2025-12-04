f444af8b3e6e8352daba8a3bb24d84923aa4dbbb
joaomarquardt <marquardt1402@gmail.com>
2025-12-04 18:22:41 -0300

    feat: add rabbit MQ configuration including exchange and queue setups

---
+++ order/src/main/java/com/api/delivery/config/RabbitMQConfig.java
@@ -0,0 +1,35 @@
+package com.api.delivery.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    public static final String ORDER_EXCHANGE = "order.exchange";
+    public static final String ROUTING_KEY = "order.created";
+    public static final String PAYMENT_STATUS_EXCHANGE = "payment.events.exchange";
+    public static final String ORDER_STATUS_QUEUE = "order.status.queue";
+
+    @Bean
+    public Exchange orderExchange() {
+        return new DirectExchange(ORDER_EXCHANGE);
+    }
+
+    @Bean
+    public Exchange paymentStatusExchange() {
+        return new FanoutExchange(PAYMENT_STATUS_EXCHANGE);
+    }
+
+    @Bean
+    public Queue orderStatusQueue() {
+        return new Queue(ORDER_STATUS_QUEUE);
+    }
+
+    @Bean
+    public Binding bindingOrderQueueToPaymentEvents(Queue orderStatusQueue, FanoutExchange paymentStatusExchange) {
+        return BindingBuilder
+                .bind(orderStatusQueue)
+                .to(paymentStatusExchange);
+    }
+}
---
+++ payment/src/main/java/com/delivery/payment/config/RabbitMQConfig.java
@@ -0,0 +1,37 @@
+package com.delivery.payment.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    public static final String PAYMENT_STATUS_EXCHANGE = "payment.events.exchange";
+    public static final String ROUTING_KEY = "payment.events.status";
+    public static final String ORDER_EXCHANGE = "order.exchange";
+    public static final String ORDER_CREATED_QUEUE = "order.created.queue";
+
+
+    @Bean
+    public Exchange paymentExchange() {
+        return new FanoutExchange(PAYMENT_STATUS_EXCHANGE);
+    }
+
+    @Bean
+    public Exchange orderExchange() {
+        return new DirectExchange(ORDER_EXCHANGE);
+    }
+
+    @Bean
+    public Queue orderCreatedQueue() {
+        return new Queue(ORDER_CREATED_QUEUE);
+    }
+
+    @Bean
+    public Binding bindingOrderCreatedQueue(Queue orderCreatedQueue, DirectExchange orderExchange) {
+        return BindingBuilder
+                .bind(orderCreatedQueue)
+                .to(orderExchange)
+                .with("");
+    }
+}
