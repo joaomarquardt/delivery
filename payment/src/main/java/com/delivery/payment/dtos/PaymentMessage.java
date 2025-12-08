919c5fad12a59cadee4b14595a457742db764019
joaomarquardt <marquardt1402@gmail.com>
2025-12-07 22:59:09 -0300

    refactor: add payment channel in dtos

--- payment/src/main/java/com/delivery/payment/dtos/PaymentMessage.java
+++ payment/src/main/java/com/delivery/payment/dtos/PaymentMessage.java
@@ -1,12 +1,14 @@
 package com.delivery.payment.dtos;
 
 import com.delivery.payment.domain.PaymentMethod;
+import com.delivery.payment.enums.PaymentChannel;
 
 import java.math.BigDecimal;
 
 public record PaymentMessage(
         Long orderId,
         PaymentMethod paymentMethod,
+        PaymentChannel paymentChannel,
         BigDecimal value,
         String cardToken
 ) {
