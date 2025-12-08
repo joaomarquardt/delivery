14c4a121711b704a17e35bc16380eba5bb88a789
joaomarquardt <marquardt1402@gmail.com>
2025-12-07 23:46:58 -0300

    fix: add message converter to fix error while sending messages between microservices

--- order/src/main/java/com/api/delivery/config/RabbitMQConfig.java
+++ order/src/main/java/com/api/delivery/config/RabbitMQConfig.java
@@ -1,6 +1,8 @@
 package com.api.delivery.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -31,5 +33,10 @@ public class RabbitMQConfig {
         return BindingBuilder
                 .bind(orderStatusQueue)
                 .to(paymentStatusExchange);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
--- payment/src/main/java/com/delivery/payment/config/RabbitMQConfig.java
+++ payment/src/main/java/com/delivery/payment/config/RabbitMQConfig.java
@@ -1,6 +1,8 @@
 package com.delivery.payment.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -33,5 +35,10 @@ public class RabbitMQConfig {
                 .bind(orderCreatedQueue)
                 .to(orderExchange)
                 .with("order.created");
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
