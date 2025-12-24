ce878f6f16ba3927bc6980c65ee7b4962d5663dc
joaomarquardt <marquardt1402@gmail.com>
2025-12-24 13:11:16 -0300

    feat: create endpoint to process payment at delivery

---
+++ payment/src/main/java/com/delivery/payment/controller/PaymentController.java
@@ -0,0 +1,24 @@
+package com.delivery.payment.controller;
+
+import com.delivery.payment.services.PaymentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payments")
+public class PaymentController {
+    private final PaymentService paymentService;
+
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping("{id}/at-delivery")
+    public ResponseEntity<Void> processAtDeliveryPayment(@PathVariable(value = "id") Long orderId) {
+        paymentService.processAtDeliveryPayment(orderId);
+        return ResponseEntity.ok().build();
+    }
+}
