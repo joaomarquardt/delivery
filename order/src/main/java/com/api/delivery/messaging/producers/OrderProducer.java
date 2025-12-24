d9460a2a9392c05c4184038268a0eb85288aff81
joaomarquardt <marquardt1402@gmail.com>
2025-12-24 09:12:48 -0300

    fix: add component annotation in order producer to be managed by spring

--- order/src/main/java/com/api/delivery/messaging/producers/OrderProducer.java
+++ order/src/main/java/com/api/delivery/messaging/producers/OrderProducer.java
@@ -3,7 +3,9 @@ package com.api.delivery.messaging.producers;
 import com.api.delivery.config.RabbitMQConfig;
 import com.api.delivery.dtos.requests.OrderPaymentMessage;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderProducer {
     private final RabbitTemplate rabbitTemplate;
 
