dde4cf7bef1e3ae7fb03d076e215a1f97057162d
joaomarquardt <marquardt1402@gmail.com>
2025-12-24 11:45:16 -0300

    refactor: add try catch to publish failed status message if necessary

--- payment/src/main/java/com/delivery/payment/messaging/listener/PaymentListener.java
+++ payment/src/main/java/com/delivery/payment/messaging/listener/PaymentListener.java
@@ -3,6 +3,7 @@ package com.delivery.payment.messaging.listener;
 import com.delivery.payment.config.RabbitMQConfig;
 import com.delivery.payment.dtos.PaymentMessage;
 import com.delivery.payment.dtos.PaymentRequest;
+import com.delivery.payment.messaging.producer.PaymentProducer;
 import com.delivery.payment.services.PaymentService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -10,9 +11,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class PaymentListener {
     private final PaymentService paymentService;
+    private final PaymentProducer paymentProducer;
 
-    public PaymentListener(PaymentService paymentService) {
+    public PaymentListener(PaymentService paymentService, PaymentProducer paymentProducer) {
         this.paymentService = paymentService;
+        this.paymentProducer = paymentProducer;
     }
 
     @RabbitListener(queues = RabbitMQConfig.ORDER_CREATED_QUEUE)
@@ -24,6 +27,11 @@ public class PaymentListener {
                 message.value(),
                 message.cardToken()
         );
-        paymentService.createPayment(paymentRequest);
+        try {
+            paymentService.validatePaymentRequest(paymentRequest);
+            paymentService.createPayment(paymentRequest);
+        } catch (Exception e) {
+            paymentProducer.sendPaymentStatusMessage(message.orderId(), false);
+        }
     }
 }
