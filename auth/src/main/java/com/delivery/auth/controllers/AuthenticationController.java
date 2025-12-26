51b260823ebf3f98d1563aad2ec2ab05895e2de1
joaomarquardt <marquardt1402@gmail.com>
2025-12-25 23:30:04 -0300

    feat: add authentication controller and service to setup login, register and logout functions

---
+++ auth/src/main/java/com/delivery/auth/controllers/AuthenticationController.java
@@ -0,0 +1,44 @@
+package com.delivery.auth.controllers;
+
+import com.delivery.auth.dtos.requests.LoginRequest;
+import com.delivery.auth.dtos.requests.RegisterRequest;
+import com.delivery.auth.dtos.responses.TokenResponse;
+import com.delivery.auth.services.AuthenticationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/auth")
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest loginRequest) {
+        TokenResponse token = authenticationService.login(loginRequest);
+        return new ResponseEntity<>(token, HttpStatus.OK);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> register(@RequestBody RegisterRequest registerRequest) {
+        authenticationService.register(registerRequest);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(@RequestHeader("Authorization") String token) {
+        authenticationService.logout(token);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    // Implementar depois
+    //@PatchMapping("/updatePassword")
+    //public ResponseEntity<Void> updatePassword(@RequestBody UpdatePasswordRequest updatePasswordRequest) {
+    //    authenticationService.updatePassword(updatePasswordRequest);
+    //    return new ResponseEntity<>(HttpStatus.OK);
+    //}
+}
